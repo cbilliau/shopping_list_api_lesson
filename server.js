ca1c3d36334434ff8a7d8429d2cbc004772b8703
cbilliau <christopher@billiau.com>
2016-07-05 16:57:37 -0400

    Refactored DELETE with mentor. Refactored PUT based on mentor's refactoring of delete.

--- server.js
+++ server.js
@@ -19,14 +19,45 @@ Storage.prototype.add = function(name) {
     return item;
 };
 
-Storage.prototype.remove = function(index) {
-    return this.items.splice(index, 1);
+Storage.prototype.remove = function(targetId) {
+
+		var targetIndex;
+
+		// Iterate over arr (items) objs & indexes
+		var targetItem = this.items.find(function(currentItem, currentIndex)	{
+
+			// var = true is obj (item) id match targetId
+			var answer = (currentItem.id === targetId);
+
+			// if var is true, indx of obj (item) in arr (items) = targetIndex
+			if (answer) {
+				targetIndex = currentIndex;
+				return answer
+			}
+		})
+
+		// if targetIndex <> undef then remove that obj
+		if (targetIndex !== undefined) this.items.splice(targetIndex, 1);
+
+		return targetItem;
 };
 
-Storage.prototype.edit = function (index, newName) {
-		var name = storage.items[index].name;
-		Object.defineProperty(storage.items[index], newName, Object.getOwnPropertyDescriptor(storage.items[index], name));
-		delete storage.items[index][name];
+Storage.prototype.edit = function (targetId, targetName) {
+
+		var targetIndex;
+
+		var targetItem = this.items.find(function(currentItem, currentIndex)	{
+			var answer = (currentItem.id == targetId);
+			if (answer) {
+				targetIndex = currentIndex;
+				return answer
+			}
+		})
+
+		// if targetIndex <> undef then replace name
+		if (targetIndex !== undefined) this.items[targetIndex].name = targetName;
+
+		return targetItem;
 };
 
 
@@ -42,7 +73,7 @@ app.use(express.static('public'));
 // GET
 app.get('/items', function(req, res) {
     res.json(storage.items);
-		console.log(storage.items);
+
 });
 
 // POST
@@ -57,45 +88,41 @@ app.post('/items', jsonParser, function(req, res) {
 // DELETE
 app.delete('/items/:id', jsonParser, function(req, res) {
 
-		// If incorrect data or id does not match a param
-    if (!req.params || storage.items[req.params.id] > -1) {
-        return res.sendStatus(400);
-    }
+		// make id an int
+		var id = +req.params.id;
+		// call remove func with id
+		var item = storage.remove(id);
 
-		// convert id to int
-    var id = +req.params.id;
-
-		// iterate over items, if match then call remove func
-    storage.items.forEach(function(val, index, arr) {
-        if (val.id === id) {
-						console.log(index);
-            var item = storage.remove(index);
-						return res.status(201).json(item);
-        }
-    });
+		// if item exists
+		if (item) {
+			// send back item
+			return res.status(201).json(item);
+		} else {
+			// else send 404
+      return res.sendStatus(404).json("Item not found.");
+		}
 });
 
 // PUT
 app.put('/items/:id', jsonParser, function(req, res) {
 
-	// If incorrect data or id does not match a param
-	if (!req.params || storage.items[req.params.id] > -1) {
-			return res.sendStatus(400);
-	}
+		// convert id to int
+		var id = +req.body.id;
+		// assign name to var
+		var name = req.body.name;
+		// call edit func with id and name
+		var item = storage.edit(id, name);
 
-	// convert id to int
-	var id = +req.body.id;
-	var name = req.body.name;
-	console.log(id, name);
-
-	// iterate over items, if match then call remove func
-	storage.items.forEach(function(val, index, arr) {
-			if (val.id === id) {
-					var item = storage.edit(index, name);
-					return res.status(201).json(item);
-			}
-	});
+		// if item exists
+		if (item) {
+			// send back item
+			return res.status(201).json(item);
+		} else {
+			// else send 404
+			return res.sendStatus(404).json("Item not found.");
+		}
 });
+
 
 // Listener
 app.listen(process.env.PORT || 8080);
